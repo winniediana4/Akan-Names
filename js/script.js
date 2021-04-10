275dd7c0569776880a4e2df2364ed46917540bbc
winniediana4 <deinawinnie84@gmail.com>
2021-04-10 17:51:26 +0300

    calculate function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var century, year, month, dayOfMonth, dayOfWeek, day;
+var century, year, month, dayOfWeek, day;
 
 function getInput(){
   century = parseInt(document.getElementById("century").value);
@@ -15,9 +15,21 @@ else if (year == ""){
   return false;
 }
 else if (month == ""){
-  alert("Input  month");
+  alert("Input month");
   return false;
 }else if(day == ""){
   alert("input date");
   return false;
+}
+function calculateDay(){
+  getInput();
+  dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7) -1;
+  console.log(dayOfWeek); 
+  return (Math.floor(dayOfWeek));
+  if (dayOfWeek < 0) {
+    dayOfWeek = dayOfWeek * -1;
+  }
+  else if (dayOfWeek > 0) {
+    return dayOfWeek;
+  }
 }
