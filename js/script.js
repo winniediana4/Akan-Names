bb8a7f8bb0083e2936393c8f40f92e5b773fc7b9
winniediana4 <deinawinnie84@gmail.com>
2021-04-10 17:37:00 +0300

    executed block of codes

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ function getInput(){
   month = parseInt(document.getElementById("month").value);
   dayOfMonth = parseInt(document.getElementById("day"))
 }
+if(century == ""){
+  alert("Input gender");
+  return false;
+}
+else if (year == ""){
+  alert("Input year");
+  return false;
+}
+else if (month == ""){
+  alert("Input  month");
+  return false;
+}else if(day == ""){
+  alert("input date");
+  return false;
+}
