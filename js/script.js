39fc2750e0a26ac807a9832022c077d6a87a8a29
winniediana4 <deinawinnie84@gmail.com>
2021-04-10 16:30:49 +0300

    call function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 var century, year, month, dayOfMonth, dayOfWeek, day;
+
+function getInput(){
+  century = parseInt(document.getElementById("century").value);
+  year = parseInt(document.getElementById("year").value);
+  month = parseInt(document.getElementById("month").value);
+  dayOfMonth = parseInt(document.getElementById("day"))
+}
