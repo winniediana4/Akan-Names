ad9e909d89f4c2ee178e84b6c226e91d172b0ae7
winniediana4 <deinawinnie84@gmail.com>
2021-04-12 09:08:20 +0300

    modify variables

--- js/script.js
+++ js/script.js
@@ -1,56 +1,7 @@
-var century, year, month, dayOfWeek, day;
-//getting input
-function getInput(){
-  century = parseInt(document.getElementById("century").value);
-  year = parseInt(document.getElementById("year").value);
-  month = parseInt(document.getElementById("month").value);
-  dayOfMonth = parseInt(document.getElementById("day"))
+function getName(){
+  var name = document.getElementById("name").value;
+var days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var akanMale = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", " Kofi", " Kwame");
+var akanFemale = ("Akosua", "Adwoa", " Abenaa", "Akua", " Yaa", "Afua", "Ama");
+var century = parseInt(document.getElementById)
 }
-if(century == ""){
-  alert("Input gender");
-  return false;
-}
-else if (year == ""){
-  alert("Input year");
-  return false;
-}
-else if (month == ""){
-  alert("Input month");
-  return false;
-}else if(day == ""){
-  alert("input date");
-  return false;
-}
-//calculating function
-function calculateDay(){
-  getInput();
-  dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7) -1;
-  console.log(dayOfWeek); //Test function
-  return (Math.floor(dayOfWeek));
-  if (dayOfWeek < 0) {
-    dayOfWeek = dayOfWeek * -1;
-  }
-  else if (dayOfWeek > 0) {
-    return dayOfWeek;
-  }
-}
-//calling main function
-function checkDayOfWeek(){
-  day = calculateDay();
-   checkGender();
-   console.log("Function works"); //Test function
-}
-//making arrays
-let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-//get  button
-function checkGender(){
-  var gen = document.getElementsByName("rads");
-  if(gen[0].checked == true){
-      var gender = "male";
-  }else if(gen[1].checked == true){
-      var gender = "female";
-  }else {
-    console.log("pass");//Test buttons
-  }
